f63444f517d79a644f695b37e70c51c60bf9277d
Natsuki <ibron1975@gamil.com>
2023-02-19 09:26:39 +0900

    initial commit

---
+++ pages/blog-page.js
@@ -0,0 +1,42 @@
+import Layout from "../components/Layout"
+import Link from "next/link"
+import { getAllPostsData } from "../lib/posts"
+import Post from "../components/Post"
+
+export default function BlogPage({ filteredPosts }) {
+  return (
+    <Layout title='Blog page'>
+      <ul>
+        {filteredPosts &&
+          filteredPosts.map((post) => <Post key={post.id} post={post} />)}
+      </ul>
+      <Link href='/main-page'>
+        <div className='flex cursor-pointer mt-12'>
+          <svg
+            xmlns='http://www.w3.org/2000/svg'
+            fill='none'
+            viewBox='0 0 24 24'
+            stroke-width='1.5'
+            stroke='currentColor'
+            class='w-6 h-6 mr-3'
+          >
+            <path
+              stroke-linecap='round'
+              stroke-linejoin='round'
+              d='M9 15L3 9m0 0l6-6M3 9h12a6 6 0 010 12h-3'
+            />
+          </svg>
+          <span>Back to main page</span>
+        </div>
+      </Link>
+    </Layout>
+  )
+}
+
+export async function getStaticProps() {
+  const filteredPosts = await getAllPostsData()
+  return {
+    props: { filteredPosts },
+    revalidate: 3,
+  }
+}
