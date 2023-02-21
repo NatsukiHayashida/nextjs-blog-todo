4c2524bc4004be9ded7ee17927bfa56634514b31
Natsuki <ibron1975@gamil.com>
2023-02-21 21:19:13 +0900

    change css2

--- pages/main-page.js
+++ pages/main-page.js
@@ -32,7 +32,7 @@ export default function MainPage() {
         viewBox='0 0 24 24'
         strokeWidth={1.5}
         stroke='currentColor'
-        className='mt-10 cursor-pointer w-6 h-6'
+        className='mt-10 cursor-pointer text-gray-500 w-10 h-8'
       >
         <path
           strokeLinecap='round'
@@ -40,6 +40,7 @@ export default function MainPage() {
           d='M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15M12 9l-3 3m0 0l3 3m-3-3h12.75'
         />
       </svg>
+      <span className="mt-1 text-xs text-gray-600">Logout</span>
     </Layout>
   )
 }
